08b6db7fa162906303162b48a30ca2a478778935
jooseeruu <arjona2908@gmail.com>
2025-02-21 23:54:18 +0100

    Lección 10 Plantillas y Componentes

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $metaTitle ?? 'Default title' }}</title> <!-- Para definir propiedades se utiliza CamelCase -->
+    <!-- Si la variable $metaTitle esta definida, entonces se va a renderizar el contenido de la variable $metaTitle, de lo contrario se va a renderizar el string 'Default title' -->
+</head>
+
+<body>
+    <x-partials.navigation />
+    <!-- Se llama al componente de Blade que se encuentra en la carpeta partials -->
+    {{ $slot }} <!-- Lo que hace es que se va a renderizar el contenido que se le pase a este componente -->
+    @isset($sidebar)   <!-- Si la variable $sidebar esta definida, entonces se va a renderizar el contenido de la variable $sidebar -->
+        <div id="SideBar">
+            <h3>SideBar</h3>
+            <div>{{ $sidebar }}</div>
+        </div>
+    @endisset
+
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
-</head>
-
-<body>
-    @include('partials.navigation') <!-- Se accede a las carpetas utilizando (.) puntos -->
+{{--@component('components.layout') --}}
+<x-layout meta-title="Home">
+    <!-- Se le pasa el valor de la variable meta-title kebab-case para referenciar propiedades -->
     <h1>Inicio</h1>
-</body>
-
-</html>
+    <x-slot:sidebar>
+        Home Sidebar
+    </x-slot:sidebar>
+</x-layout>
+{{-- @endcomponent --}}
