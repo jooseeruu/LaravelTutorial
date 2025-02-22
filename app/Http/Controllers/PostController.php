29885b519f7332e281e76185a5c5b0d99a4e0644
jooseeruu <arjona2908@gmail.com>
2025-02-22 23:34:35 +0100

    Lección 14 ORM Eloquent

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,12 +1,13 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
+use App\Models\Post; // importar el modelo Post
 
 class PostController extends Controller
 {
     public function index() // __Invoke solo Util cuando solo se tiene un metodo en el controlador
     {
-        $posts = DB::table('posts')->get(); // Obtener todos los registros de la tabla posts
+        $posts = Post::get(); // Cambiar a DB::table('posts')->get(); para usar Query Builder
 
         return view('blog', ['posts' => $posts]);
     }
