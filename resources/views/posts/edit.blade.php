adab64fa24eabe15e79a7ae3364b70347093bc34
jooseeruu <arjona2908@gmail.com>
2025-02-23 22:51:04 +0100

    Lección 21 formulario para editar

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,10 +1,11 @@
-<x-layout meta-title="Create new post" meta-description="Form to create a new post">
-    <h1>{{ __('Create new post') }}</h1>
-    <form action="{{ route('posts.store') }}" method="POST">
-        @csrf
+<x-layout :meta-title="$post->title" :meta-description="$post->body">
+    <h1>Edit form</h1>
+
+    <form action="{{ route('posts.update', $post) }}" method="POST">
+        @csrf @method('PATCH')
         <label>
             {{ __('Title') }} <br />
-            <input name="title" type="text" value="{{ old('title') }}" />
+            <input name="title" type="text" value="{{ old('title', $post->title) }}" />
             @error('title')
                 <br />
                 <small style="color: red">{{ $message }}</small>
@@ -12,7 +13,7 @@
         </label> <br />
         <label>
             {{ __('Body') }} <br />
-            <textarea name="body">{{old('body')}}</textarea>
+            <textarea name="body">{{old('body', $post->body)}}</textarea>
             @error('body')
                 <br />
                 <small style="color: red">{{ $message }}</small>
@@ -22,5 +23,6 @@
         <br />
     </form>
     <br />
-    <a href="{{ route('posts.index') }}">{{ __('Back') }}</a>
+
+    <a href="{{ route('posts.index') }}">Back</a>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/blog', [PostController::class, 'index'])->name('posts.index');
 Route::get('/blog/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/blog', [PostController::class, 'store'])->name('posts.store');
 Route::get('/blog/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/blog/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::patch('/blog/{post}', [PostController::class, 'update'])->name('posts.update');
 
 // Utilizamos Controladores para manejar la lógica de la aplicación
 // Usamos el método 'index' del controlador PostController para manejar la lógica de la página 'blog'.
