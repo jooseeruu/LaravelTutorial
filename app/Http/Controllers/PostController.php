49303f0e008660d7daa00f686f1eff10b60f952c
jooseeruu <arjona2908@gmail.com>
2025-02-23 23:46:40 +0100

    Lección 25 Delete laravel

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -43,4 +43,10 @@ class PostController extends Controller
         return to_route('posts.show', $post)->with('status', 'Post updated!');
 
     }
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return to_route('posts.index')->with('status', 'Post deleted!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,12 @@ Route::view('/', 'welcome')->name('home');
 Route::view('contacto', 'contact')->name('contact');
 
 
-Route::get('/blog', [PostController::class, 'index'])->name('posts.index');
-Route::get('/blog/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/blog', [PostController::class, 'store'])->name('posts.store');
-Route::get('/blog/{post}', [PostController::class, 'show'])->name('posts.show');
-Route::get('/blog/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::patch('/blog/{post}', [PostController::class, 'update'])->name('posts.update');
+Route::resource('blog', PostController::class, [
+    'names' => 'posts',
+    'parameters' => [
+        'blog' => 'post',
+    ],
+]);
 
 // Utilizamos Controladores para manejar la lógica de la aplicación
 // Usamos el método 'index' del controlador PostController para manejar la lógica de la página 'blog'.
